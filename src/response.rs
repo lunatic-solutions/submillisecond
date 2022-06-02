f0c0ca925cb72d1b5004d649228e92293f430c24
Ari Seyhun <ariseyhun@live.com.au>
2022-06-02 20:00:11 +0930

    Implement `IntoResponse` for types

--- src/response.rs
+++ src/response.rs
@@ -1,5 +1,85 @@
-use http::Response;
+//! TODO response documentation
 
+use std::borrow::Cow;
+
+use http::StatusCode;
+
+/// Type alias for [`http::Response`] whose body defaults to [`Vec<u8>`].
+pub type Response<T = Vec<u8>> = http::Response<T>;
+
+/// Converts a type into a [`Response`].
+///
+/// Types implementing `IntoResponse` can be returned from handlers.
 pub trait IntoResponse {
-    fn into_response(self) -> Response<Vec<u8>>;
+    /// Create a response.
+    fn into_response(self) -> Response;
+}
+
+impl IntoResponse for Response {
+    fn into_response(self) -> Response {
+        self
+    }
+}
+
+impl IntoResponse for () {
+    fn into_response(self) -> Response {
+        Response::default()
+    }
+}
+
+impl IntoResponse for &[u8] {
+    fn into_response(self) -> Response {
+        Response::new(self.to_vec())
+    }
+}
+
+impl IntoResponse for Cow<'static, [u8]> {
+    fn into_response(self) -> Response {
+        Response::new(self.to_vec())
+    }
+}
+
+impl IntoResponse for &str {
+    fn into_response(self) -> Response {
+        Response::new(self.as_bytes().to_vec())
+    }
+}
+
+impl IntoResponse for Cow<'static, str> {
+    fn into_response(self) -> Response {
+        Response::new(self.as_bytes().to_vec())
+    }
+}
+
+impl IntoResponse for Vec<u8> {
+    fn into_response(self) -> Response {
+        Response::new(self)
+    }
+}
+
+impl IntoResponse for String {
+    fn into_response(self) -> Response {
+        Response::new(self.into_bytes())
+    }
+}
+
+impl<T, E> IntoResponse for Result<T, E>
+where
+    T: IntoResponse,
+    E: IntoResponse,
+{
+    fn into_response(self) -> Response {
+        match self {
+            Ok(value) => value.into_response(),
+            Err(err) => err.into_response(),
+        }
+    }
+}
+
+impl IntoResponse for StatusCode {
+    fn into_response(self) -> Response {
+        let mut res = ().into_response();
+        *res.status_mut() = self;
+        res
+    }
 }
