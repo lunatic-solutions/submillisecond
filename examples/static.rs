637f4d2200081d28e5a0dd109d4bdc7cc9f2535b
Ari Seyhun <ariseyhun@live.com.au>
2022-06-21 17:10:31 +0930

    Format static example

--- examples/static.rs
+++ examples/static.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use submillisecond::{router, Application, static_dir};
+use submillisecond::{router, static_dir, Application};
 
 fn main() -> io::Result<()> {
     Application::new(router! {
