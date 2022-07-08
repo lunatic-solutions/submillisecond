85c7da6c7d80d4e32cfc903e4ed0375e11209bff
Ari Seyhun <ariseyhun@live.com.au>
2022-07-08 14:11:20 +0930

    Rename `static_dir!` to `static_router!`

--- examples/static.rs
+++ examples/static.rs
@@ -1,10 +1,7 @@
 use std::io;
 
-use submillisecond::{router, static_dir, Application};
+use submillisecond::{static_router, Application};
 
 fn main() -> io::Result<()> {
-    Application::new(router! {
-        "/" => static_dir!("./static")
-    })
-    .serve("0.0.0.0:3000")
+    Application::new(static_router!("./static")).serve("0.0.0.0:3000")
 }
