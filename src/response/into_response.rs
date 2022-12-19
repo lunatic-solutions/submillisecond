83121a9ee84e3f378991c37fde355d065d06be6c
Mads Hougesen <madshougesen@gmail.com>
2022-12-19 13:33:54 +0100

    feat(response): add Redirect response (#101)

--- src/response/into_response.rs
+++ src/response/into_response.rs
@@ -179,6 +179,101 @@ where
     }
 }
 
+/// Response for redirecting the client
+///
+/// # Example
+/// ```
+/// use submillisecond::http::Uri;
+/// Redirect::to(Uri::from_static("https://lunatic.solutions/"));
+/// ```
+pub struct Redirect {
+    /// Uri to redirect to
+    pub uri: http::Uri,
+    status_code: StatusCode,
+}
+
+impl Redirect {
+    /// Constructs a **303 See Other** redirect response.
+    ///
+    /// Instructs the client to issue a GET request to the supplied URI.
+    ///
+    /// [[RFC7231, Section 6.4.4](https://tools.ietf.org/html/rfc7231#section-6.4.4)]
+    pub fn to(uri: http::Uri) -> Self {
+        Redirect {
+            uri,
+            status_code: StatusCode::SEE_OTHER,
+        }
+    }
+
+    /// Constructs a **307 Temporary Redirect** redirect response.
+    ///
+    /// Instructs the client to reissue the original request to the supplied URI, while keeping the original HTTP method and request content.
+    ///
+    /// [[RFC7231, Section 6.4.7](https://tools.ietf.org/html/rfc7231#section-6.4.7)]
+    pub fn temporary(uri: http::Uri) -> Self {
+        Redirect {
+            uri,
+            status_code: StatusCode::TEMPORARY_REDIRECT,
+        }
+    }
+
+    /// Constructs a **308 Permanent Redirect** redirect response.
+    ///
+    /// Instructs the client to reissue the original request to the supplied URI, while keeping the original HTTP method and request content.
+    ///
+    /// Should only be used for permanent redirects, since it may be cached by the client.
+    ///
+    /// [[RFC7238](https://tools.ietf.org/html/rfc7238)]
+    pub fn permanent(uri: http::Uri) -> Self {
+        Redirect {
+            uri,
+            status_code: StatusCode::PERMANENT_REDIRECT,
+        }
+    }
+}
+
+impl IntoResponse for Redirect {
+    fn into_response(self) -> Response {
+        Response::builder()
+            .status(self.status_code)
+            .header("Location", self.uri.to_string())
+            .body(Vec::new())
+            .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{IntoResponse, Redirect};
+
+    #[test]
+    fn redirect() {
+        let uri = "https://lunatic.solutions/";
+
+        let base_redirect = Redirect::to(http::Uri::from_static(uri)).into_response();
+        let temporary_redirect = Redirect::temporary(http::Uri::from_static(uri)).into_response();
+        let permanent_redirect = Redirect::permanent(http::Uri::from_static(uri)).into_response();
+
+        assert_eq!(base_redirect.status(), http::StatusCode::SEE_OTHER);
+        assert_eq!(
+            temporary_redirect.status(),
+            http::StatusCode::TEMPORARY_REDIRECT
+        );
+        assert_eq!(
+            permanent_redirect.status(),
+            http::StatusCode::PERMANENT_REDIRECT
+        );
+
+        assert!(base_redirect.headers().contains_key("Location"));
+        assert!(temporary_redirect.headers().contains_key("Location"));
+        assert!(permanent_redirect.headers().contains_key("Location"));
+
+        assert_eq!(base_redirect.headers().get("Location").unwrap(), uri);
+        assert_eq!(temporary_redirect.headers().get("Location").unwrap(), uri);
+        assert_eq!(permanent_redirect.headers().get("Location").unwrap(), uri);
+    }
+}
+
 macro_rules! impl_into_response {
     ( $($ty:ident),* $(,)? ) => {
         #[allow(non_snake_case)]
